984e50770101de9867f34bd78b609a848ed1939c
NarendraS2001 <141475729+NarendraS2001@users.noreply.github.com>
2023-08-05 11:11:10 +0530

    Add files via upload

---
+++ src/main/java/com/eCommerce/management/Model/Order.java
@@ -0,0 +1,27 @@
+package com.eCommerce.management.Model;
+
+import com.eCommerce.management.Enum.OrderType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document (collection = "Order")
+public class Order {
+    @Id
+    private String orderId;
+    private String itemId;
+    private String itemName;
+    private int quantity;
+    private OrderType orderType;
+    private LocalDateTime localDateTime;
+
+}
